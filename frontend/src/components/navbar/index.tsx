0b476bcf08812befbbace0cde084f5b14669c66e
GuilhermeSeveriano <guiseveriano.10@gmail.com>
2022-02-07 09:58:42 -0300

    Mudança Layout

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     <header>
       <nav className="container">
         <div className="dsmovie-nav-content">
-          <h1>DSMovie</h1>
+          <h1>SevsMovie</h1>
           <a
             href="https://github.com/Gustavo-Apolonio"
             target="_blank"
